e20e3e746e012df0b45997f0b69b759b5bd5cfff
jlnshly <jelaineashleyramos@gmail.com>
2026-06-04 03:19:26 +0800

    adding main

--- demo_car.py
+++ demo_car.py
@@ -1,2 +1,11 @@
 import time
 from car import Car
+
+
+def main():
+    my_car = Car(2024, "Toyota Supra")
+
+    print("=" * 45)
+    print(f"Starting engine for the {my_car.get_car_info()}")
+    print("=" * 45)
+    time.sleep(1)
